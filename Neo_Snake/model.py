9a492a896f56ef6870e20d055bee373e0e14f35a
Mariana <mhenaoecheverri@gmail.com>
2025-04-07 10:59:00 -0500

    Implementación clase Cabeza y métodos init, mover y cambiar_dirección.

--- Neo_Snake/model.py
+++ Neo_Snake/model.py
@@ -4,3 +4,25 @@ import sys
 TAMANO_CELDA = 30
 ANCHO, ALTO = 600, 600
 FPS = 10
+
+class Cabeza:
+    def __init__(self, x, y):
+        self.posicion = (x, y)
+        self.direccion = "DERECHA"
+
+    def mover(self):
+        x, y = self.posicion
+        if self.direccion == "ARRIBA":
+            y -= 1
+        elif self.direccion == "ABAJO":
+            y += 1
+        elif self.direccion == "IZQUIERDA":
+            x -= 1
+        elif self.direccion == "DERECHA":
+            x += 1
+        self.posicion = (x, y)
+
+    def cambiar_direccion(self, nueva_direccion):
+        opuestos = {"ARRIBA": "ABAJO", "ABAJO": "ARRIBA", "IZQUIERDA": "DERECHA", "DERECHA": "IZQUIERDA"}
+        if nueva_direccion != opuestos.get(self.direccion, None):
+            self.direccion = nueva_direccion
